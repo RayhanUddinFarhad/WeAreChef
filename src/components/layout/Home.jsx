9293b83dc992105ddaf0bfab202516ac7232d0ce
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-02 01:11:14 +0600

    added customer review section

--- src/components/layout/Home.jsx
+++ src/components/layout/Home.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Banner from '../pages/Banner';
 import Chefs from '../pages/Chefs';
+import CustomerReview from '../pages/CustomerReview';
 
 const Home = () => {
     return (
@@ -9,6 +10,8 @@ const Home = () => {
             <Banner></Banner>
 
             <Chefs></Chefs>
+
+            <CustomerReview></CustomerReview>
             
         </div>
     );
---
+++ src/components/pages/CustomerReview.jsx
@@ -0,0 +1,26 @@
+import React from 'react';
+
+const CustomerReview = () => {
+    return (
+        <div className='my-10'>
+
+<h1 className='text-4xl font-semibold  border-green-600 text-center'>Customers <span className='text-green-600 font-bold'>Reviews</span>
+            <hr className=' bg-green-600 mx-96 text-center my-2 h-1' />
+
+            
+            </h1>
+
+<div className="card card-side bg-base-100 shadow-xl">
+  <figure><img className='h-96' src="https://i.ibb.co/KDnZ1SB/MYXJ-20220419103015094-save.jpg" alt="Movie"/></figure>
+  <div className="card-body">
+    <h2 className="text-center font-bold text-3xl">Rayhan Uddin Farhad</h2>
+    <p>"I recently used this website to find chef and I have to say, <br /> I particularly appreciated the chef profiles, <br /> which allowed me to learn more about the people behind the recipes. <br /> I found the search function to be very helpful and was able to quickly find what I was looking for. <br /> Overall, I'm very satisfied with the service I received and would definitely recommend this website to anyone looking for delicious recipes and cooking inspiration!"</p>
+   
+  </div>
+</div>
+            
+        </div>
+    );
+};
+
+export default CustomerReview;
