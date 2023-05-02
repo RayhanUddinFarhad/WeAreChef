27733fe3bc4a13d369f224d6803626a6e9694b75
ITP <RayhanUddinFarhad11@gmail.com>
2023-05-02 12:17:52 +0600

    Rating added

--- src/components/pages/RecipeCard.jsx
+++ src/components/pages/RecipeCard.jsx
@@ -1,4 +1,7 @@
+import { Rating } from '@smastrom/react-rating';
 import React from 'react';
+import '@smastrom/react-rating/style.css'
+
 
 const RecipeCard = ({ data }) => {
 
@@ -9,7 +12,7 @@ const RecipeCard = ({ data }) => {
 <div className="card  bg-base-100 shadow-xl">
   <figure><img className='w-96' src= {data.recipePicture} alt="Shoes" /></figure>
   <div className="card-body space-y-5">
-    <h2 className="card-title">
+    <h2 className="text-4xl font-extrabold text-center">
       {data.recipeName}
 
 
@@ -31,8 +34,15 @@ data.ingredients && data.ingredients.map ( (ing) =>       <p className=" bg-oran
   </div>
     <p> <span className='font-bold text-2xl px-10 '>Cooking Method:</span>   {data.cookingMethod}</p>
     <div className="card-actions justify-end">
-      <div className="badge badge-outline">Fashion</div>
-      <div className="badge badge-outline">Products</div>
+      <p className='flex items-center'>
+
+        <span className='text-lg font-bold mr-5'>{data.rating}</span>
+        <Rating
+      style={{ maxWidth: 180 }}
+      value={data.rating}
+      readOnly
+    />
+      </p>
     </div>
   </div>
 </div>
